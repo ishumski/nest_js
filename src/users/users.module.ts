f375a2683bbfc4e67f2b0af4cccfc9c40b54f1a8
Ilya Shumski <ilya.shumski@exposit.com>
2022-05-19 16:01:14 +0300

    add roles and connections

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,15 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import {SequelizeModule} from "@nestjs/sequelize";
 import {User} from "./users.model";
+import {Role} from "../roles/roles.model";
+import {RolesModule} from "../roles/roles.module";
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
-  imports: [SequelizeModule.forFeature([User])]
+  imports: [
+      SequelizeModule.forFeature([User, Role]),
+      RolesModule
+  ]
 })
 export class UsersModule {}
